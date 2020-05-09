36658899626c8dede689071a223da1d82d5be4e7
Mk <hawhaw4966@gmail.com>
2020-05-09 21:34:25 +0900

    scraping

---
+++ yamanote.js
@@ -0,0 +1,22 @@
+'use strcit';
+
+const axios = require('axios');
+
+// 実際にデータを取得する getRequest 関数
+async function getRequest() {
+  let response;
+  try {
+    response = await axios.get('https://transit.yahoo.co.jp/traininfo/detail/21/0/');
+    let html = response.data;
+    html = html.replace(/\r?\n/g,"");//整形1: 改行などを削除して整形しやすくする
+    let unko = html.match(/id="mdServiceStatus">(.*?)<\/div>/)[1];
+    console.log(unko);
+
+    await updateData({msg: unko}); //データ更新関数を実行
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getRequest を呼び出してデータを読み込む
+getRequest();
